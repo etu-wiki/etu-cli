b4731a4f538e5d8f910d516b970eace1f98ff9d8
joesong168 <joesong168@qq.com>
2022-04-23 19:49:39 +0800

    initial push

---
+++ bin/etu.js
@@ -0,0 +1,254 @@
+#!/usr/bin/env node
+
+// Native
+import { createServer as createHttpServer } from "http";
+import { createServer as createSecureHttpSever } from "https";
+import { resolve } from "path";
+import { rmSync, existsSync, unlinkSync, readFileSync } from "fs";
+import { promisify } from "util";
+import { networkInterfaces, homedir } from "os";
+import open from "open";
+
+// Packages
+import chalk from "chalk";
+import arg from "arg";
+import handler from "serve-handler";
+import boxen from "boxen";
+import compression from "compression";
+import generateIIIF from "./iiif.js";
+
+// Utilities
+const compressionHandler = promisify(compression());
+const interfaces = networkInterfaces();
+
+const warning = (message) => `${chalk.yellow(" WARNING:")} ${message}`;
+const info = (message) => `${chalk.magenta(" INFO:")} ${message}`;
+const error = (message) => `${chalk.red(" ERROR:")} ${message}`;
+
+let url;
+const ETU_PATH = homedir + "/etu/";
+
+const getHelp = () => chalk`
+  eut - present your IIIF image on the fly
+
+  USAGE
+
+      $ etu --help
+      $ etu [image_folder | image_file]
+      $ etu [-p listen_port] [image_folder | image_file]
+
+      By default, etu will listen on localhost:3000 and serving the
+      current working directory on that address.
+
+  OPTIONS
+
+      --help                              Shows this help message
+
+      -p, --port                          Specify a port on which to listen
+
+      -C, --cors                          Enable CORS, sets \`Access-Control-Allow-Origin\` to \`*\`
+	  
+	  --ssl-cert                          Optional path to an SSL/TLS certificate to etu with HTTPS
+	  
+	  --ssl-key                           Optional path to the SSL/TLS certificate\'s private key
+
+      --no-port-switching                 Do not open a port other than the one specified when it\'s taken.
+`;
+
+const clearWorkingFolder = () => {
+  rmSync(ETU_PATH + "i", { recursive: true, force: true });
+  rmSync(ETU_PATH + "p", { recursive: true, force: true });
+  if (existsSync(ETU_PATH + "viewer")) {
+    unlinkSync(ETU_PATH + "viewer");
+  }
+};
+
+const registerShutdown = (fn) => {
+  let run = false;
+
+  const wrapper = () => {
+    if (!run) {
+      run = true;
+      fn();
+    }
+  };
+
+  process.on("SIGINT", wrapper);
+  process.on("SIGTERM", wrapper);
+  process.on("exit", wrapper);
+};
+
+const getNetworkAddress = () => {
+  for (const name of Object.keys(interfaces)) {
+    for (const inter of interfaces[name]) {
+      const { address, family, internal } = inter;
+      if (family === "IPv4" && !internal) {
+        return address;
+      }
+    }
+  }
+};
+
+const startEndpoint = async (port, config, args, previous) => {
+  const start = Date.now();
+
+  const cwd = process.cwd();
+  const entry = args._.length > 0 ? resolve(args._[0]) : cwd;
+
+  let viewer = args["--viewer"] || "m3";
+  let viewerName = "Mirador 3";
+  let iiifVersion = 3;
+
+  // Nevigate to index if file not found
+  config.rewrites = [
+    {
+      source: "**",
+      destination: "/viewer/index.html",
+    },
+  ];
+
+  const { isTTY } = process.stdout;
+  const httpMode = args["--ssl-cert"] && args["--ssl-key"] ? "https" : "http";
+
+  const severHandler = async (request, response) => {
+    if (args["--cors"]) {
+      response.setHeader("Access-Control-Allow-Origin", "*");
+    }
+    await compressionHandler(request, response);
+
+    return handler(request, response, config);
+  };
+
+  const server =
+    httpMode === "https"
+      ? createSecureHttpSever(
+          {
+            key: readFileSync(args["--ssl-key"]),
+            cert: readFileSync(args["--ssl-cert"]),
+          },
+          severHandler
+        )
+      : createHttpServer(severHandler);
+
+  server.on("error", (err) => {
+    if (
+      err.code === "EADDRINUSE" &&
+      !isNaN(port) &&
+      args["--no-port-switching"] !== true
+    ) {
+      startEndpoint(0, config, args, port);
+      return;
+    }
+
+    console.error(error(`Failed to etu: ${err.stack}`));
+    process.exit(1);
+  });
+
+  server.listen(port, async () => {
+    const details = server.address();
+    registerShutdown(() => server.close());
+
+    let localAddress = null;
+    let networkAddress = null;
+
+    if (typeof details === "string") {
+      localAddress = details;
+    } else if (typeof details === "object" && details.port) {
+      const address = details.address === "::" ? "localhost" : details.address;
+      const ip = getNetworkAddress();
+
+      localAddress = `${httpMode}://${address}:${details.port}`;
+      networkAddress = ip ? `${httpMode}://${ip}:${details.port}` : null;
+    }
+    console.log(info(`Accepting connections on ${localAddress}`));
+    try {
+      // generate IIIF content
+      url = await generateIIIF(entry, viewer, iiifVersion, localAddress);
+      open(url);
+      const stop = Date.now();
+      if (isTTY && process.env.NODE_ENV !== "production") {
+        let message = chalk.green("Present your IIIF image on the fly!\n");
+        message += `\n${chalk.bold("- Time Cost:   ")}  ${
+          (stop - start) / 1000
+        } seconds`;
+        message += `\n${chalk.bold("- IIIF Viewer: ")}  ${viewerName}`;
+        message += `\n${chalk.bold("- IIIF Version:")}  ${iiifVersion}`;
+        message += `\n${chalk.bold("- Viewer Url:  ")}  ${url}`;
+        if (previous) {
+          message += chalk.red(
+            `\n\nThis port was picked because ${chalk.underline(
+              previous
+            )} is in use.`
+          );
+        }
+        console.log(
+          boxen(message, {
+            padding: 1,
+            borderColor: "green",
+            margin: 1,
+          })
+        );
+      } else {
+        const suffix = localAddress ? ` at ${localAddress}` : "";
+        console.log(info(`Accepting connections${suffix}`));
+      }
+    } catch (err) {
+      console.log(error(err.message));
+    }
+  });
+};
+
+clearWorkingFolder();
+
+let args = null;
+
+try {
+  args = arg({
+    "--help": Boolean,
+    "--viewer": String,
+    "--port": String,
+    "--cors": Boolean,
+    "--no-port-switching": Boolean,
+    "--ssl-cert": String,
+    "--ssl-key": String,
+    "-h": "--help",
+    "-v": "--viewer",
+    "-C": "--cors",
+    "-p": "--port",
+  });
+} catch (err) {
+  console.error(error(err.message));
+  process.exit(1);
+}
+
+if (args["--help"]) {
+  console.log(getHelp());
+  process.exit(0);;
+}
+
+if (!args["--port"]) {
+  // Default endpoint
+  args["--port"] = process.env.PORT || 3000;
+}
+
+if (args._.length > 1) {
+  console.error(error("Please provide one path argument at maximum"));
+  process.exit(1);
+}
+
+const config = {};
+config.public = ETU_PATH;
+config.etag = true;
+config.symlinks = true;
+config.cleanUrls = false;
+
+await startEndpoint(args["--port"], config, args);
+
+registerShutdown(() => {
+  console.log(`\n${info("Gracefully shutting down. Please wait...")}`);
+  clearWorkingFolder();
+  process.on("SIGINT", () => {
+    console.log(`\n${warning("Force-closing all open sockets...")}`);
+    process.exit(0);
+  });
+});
---
+++ bin/iiif.js
@@ -0,0 +1,122 @@
+import fs from "fs";
+import os from "os";
+import path from "path";
+
+import ora from "ora";
+import mkdir from "make-dir";
+import Mustache from "mustache";
+import { v4 as uuid } from "uuid";
+
+import chalk from "chalk";
+const warning = (message) => `${chalk.yellow(" WARNING:")} ${message}`;
+const info = (message) => `${chalk.magenta(" INFO:")} ${message}`;
+const error = (message) => `${chalk.red(" ERROR:")} ${message}`;
+
+import Jimp from "jimp";
+
+const ETU_PATH = os.homedir + "/etu/";
+
+function addLastMark(arr) {
+  return arr.map((e, i, a) => {
+    if (a.length === i + 1) {
+      e.last = true;
+    }
+    return e;
+  });
+}
+
+export default async function generateIIIF(
+  entry,
+  viewer,
+  iiifVersion,
+  baseUrl
+) {
+  const presentUuid = uuid();
+  const __dirname = path.resolve();
+
+  const model = {
+    baseUrl,
+    presentUuid,
+  };
+
+  const items = [];
+  let files;
+  if (fs.lstatSync(entry).isDirectory()) {
+    files = fs.readdirSync(entry);
+  } else {
+    files = [entry];
+  }
+  for (const file of files) {
+    const sourceFile = path.resolve(entry, file);
+    console.log(info(`Convert ${sourceFile}`));
+    const item = {};
+
+    // Code for jimp
+    let meta, image;
+    try {
+      image = await Jimp.read(sourceFile);
+      meta = {
+        width: image.bitmap.width,
+        height: image.bitmap.height,
+        format: image.getMIME(),
+      };
+    } catch (err) {
+      console.error(err.message);
+      throw Error("Your file is too large to process.");
+    }
+
+    if (meta && meta.height && meta.width) {
+      item.label = path.basename(file, path.extname(file));
+      item.height = meta.height;
+      item.width = meta.width;
+      item.canvasUuid = uuid();
+      item.etag = uuid();
+
+      const imageFolder = ETU_PATH + "i/" + iiifVersion + "/";
+      mkdir.sync(imageFolder);
+
+      // Code for Jimp
+      item.level0 = true;
+      const fullImage = imageFolder + item.etag + ".jpg";
+      if (meta.format !== Jimp.MIME_JPEG) {
+        const spinner = ora(`Converting ${sourceFile}`).start();
+        image.quality(75).write(fullImage);
+        spinner.stop();
+      } else {
+        fs.copyFileSync(sourceFile, fullImage);
+      }
+
+      items.push(item);
+    }
+  }
+
+  model.items = addLastMark(items);
+  if (items.length > 1) {
+    model.label = path.basename(entry);
+  } else if (items.length === 1) {
+    model.label = items[0].label;
+  }
+
+  const template = fs
+    .readFileSync(__dirname + "/template/present" + iiifVersion + ".mustache")
+    .toString();
+  const present = Mustache.render(template, model);
+
+  const presentFolder = ETU_PATH + "p/" + iiifVersion + "/";
+  mkdir.sync(presentFolder);
+  fs.writeFileSync(presentFolder + presentUuid + ".json", present);
+
+  fs.symlinkSync(__dirname + "/viewer/" + viewer, ETU_PATH + "viewer", "dir");
+
+  const url =
+    model.baseUrl +
+    "/viewer/index.html?manifest=" +
+    model.baseUrl +
+    "/p/" +
+    iiifVersion +
+    "/" +
+    presentUuid +
+    ".json";
+  // console.log(url);
+  return url;
+}
