d3ae6c7b3f5a28fbb6ee2eac240a2eb60e6e859c
joesong168 <joesong168@qq.com>
2023-06-19 13:22:44 +0800

    dev config

--- src/config.mjs
+++ src/config.mjs
@@ -1,10 +1,10 @@
-export const USER_POOL_ID = "us-east-1_prfVWwbqe"
-export const IDENTITY_POOL_ID = "us-east-1:4a5a4801-27b6-4f47-9505-6742eba82ab8"
-export const COGNITO_CLIENT_ID = "4unmnren3vtqcd32am19o2js0l"
-export const UPLOAD_BUCKET = "prodgl-upload"
-export const TENANT_TABLE = "prodgl-tenant"
-export const IMAGE_API_ENDPOINT = 'https://image.iiify.com'
-export const ADMIN_API_ENDPOINT = "https://kciiqwtr90.execute-api.us-east-1.amazonaws.com/prod"
+export const USER_POOL_ID = "us-east-1_eWW4WQ3sQ"
+export const IDENTITY_POOL_ID = "us-east-1:34efad88-c119-419d-b132-5daed4e8b978"
+export const COGNITO_CLIENT_ID = "2vsgejput2aukgbj7fv3irfd3j"
+export const UPLOAD_BUCKET = "devgl-upload"
+export const TENANT_TABLE = "devgl-tenant"
+export const IMAGE_API_ENDPOINT = 'https://devgl.image.fiii.cc/image'
+export const ADMIN_API_ENDPOINT = "https://albb7qakl0.execute-api.us-east-1.amazonaws.com/dev"
 
 export const AWS_REGION = "us-east-1"
 export const THUMB_WIDTH_THRESHOLD = 1000 * 4;
